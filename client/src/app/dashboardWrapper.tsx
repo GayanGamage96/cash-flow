ae29a3ee29c636698b781541d835b23de5f6fe94
GayanGamage96 <gayangamagercg1996@gmail.com>
2025-03-02 00:57:33 +0530

    frontend- dashboard layout, initial navbar structure have been implemented

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* SIDEBAR */}
+      Sidebar
+      <main
+        className={`flex w-full flex-col bg-gray-50 md:pl-64 dark:bg-dark-bg`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
