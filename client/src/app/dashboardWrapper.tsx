421bbf3f803239a1deb04f064269a8d902682dfa
GayanGamage96 <gayangamagercg1996@gmail.com>
2025-03-02 13:00:37 +0530

    frontend:setup Redux store with RTK and global state management

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,20 +1,29 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import React from "react";
+import StoreProvider from "@/state/redux";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* SIDEBAR */}
 
       <Sidebar />
       <main
-        className={`flex w-full flex-col bg-gray-50 md:pl-64 dark:bg-dark-bg`}
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
---
+++ client/src/state/redux.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { Provider } from "react-redux";
+import { useRef } from "react";
+import globalReducer from "@/state";
+
+/*REDUX STORE*/
+const rootReducer = combineReducers({
+  global: globalReducer,
+});
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+  });
+};
+
+/* REDUX TYPES */
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+/* PROVIDER */
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const storeRef = useRef<AppStore | null>(null);
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+    setupListeners(storeRef.current.dispatch);
+  }
+  return <Provider store={storeRef.current}>{children}</Provider>;
+}
